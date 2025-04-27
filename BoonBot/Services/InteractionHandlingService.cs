70cfb04e3875b3db35dcb98c19f4706009fae5a6
ITOKONPSP-PC\itoko <itokonpsp@gmail.com>
2025-04-27 23:26:55 +0900

    欠けていたファイルを追加

---
+++ BoonBot/Services/InteractionHandlingService.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace BoonBot.Services;
+
+public class InteractionHandlingService
+{
+    private readonly DiscordSocketClient _discordClient;
+    private readonly InteractionService _interaction;
+    private readonly IServiceProvider _services;
+    private readonly IConfiguration _configuration;
+    
+    public InteractionHandlingService(DiscordSocketClient discordClient, InteractionService interaction, IServiceProvider services, IConfiguration configuration)
+    {
+        _discordClient = discordClient;
+        _interaction = interaction;
+        _services = services;
+        _configuration = configuration;
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _interaction.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+    }
+}
